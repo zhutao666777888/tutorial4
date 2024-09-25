b977b5cd7bf255a6d2224747d8285e2eb155c70a
Zhu Tao <2573779675@qq.com>
2024-09-25 17:52:55 +0800

    Add subtraction test

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -10,5 +10,10 @@ public class CalcTest {
     void testAddition() {
         assertEquals(4, c.add(2,2));
     }
+    @Test
+    void testSubtraction() {
+        assertEquals(2, c.subtract(4, 2));
+    }
 
 }
+
